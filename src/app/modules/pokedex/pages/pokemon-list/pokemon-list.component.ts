e0c963d0eb3a20590c4eca768fc39aa790214b15
Daniel Hernandez <dahernandez@intelix.biz>
2024-04-18 16:38:06 -0400

    Switchmap operator in pokemon list for optimization

--- src/app/modules/pokedex/pages/pokemon-list/pokemon-list.component.ts
+++ src/app/modules/pokedex/pages/pokemon-list/pokemon-list.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '@services/pokemon.service';
 //models
 import { paginator, previewInfo } from '@models/paginator-variables.interface';
-import { ListedPokemon, PreviewPokemon } from '@models/listed-pokemon.interface';
-import { BehaviorSubject, take, tap } from 'rxjs';
+import {
+  ListedPokemon,
+  PreviewPokemon,
+} from '@models/listed-pokemon.interface';
+import { BehaviorSubject, of, switchMap, take, tap } from 'rxjs';
 //environment
 import { environment } from '@environments/environment';
 //utils
@@ -13,122 +16,150 @@ import { pkmnStatsToArray } from '@shared/utils/pokemon-utils';
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.scss']
+  styleUrls: ['./pokemon-list.component.scss'],
 })
 export class PokemonListComponent implements OnInit {
-
-  private pokemonListSubject = new BehaviorSubject<ListedPokemon['species'] | null>(null);
+  private pokemonListSubject = new BehaviorSubject<
+    ListedPokemon['species'] | null
+  >(null);
   pokemonList$ = this.pokemonListSubject.asObservable();
 
-  private previewPokemonSubject = new BehaviorSubject<PreviewPokemon | null>(null);
+  private previewPokemonSubject = new BehaviorSubject<PreviewPokemon | null>(
+    null
+  );
   previewPokemon$ = this.previewPokemonSubject.asObservable();
 
   page_info: paginator = {
     limit: 10,
     offset: 0,
-    max: environment['fetch_pkmn']
+    max: environment['fetch_pkmn'],
   };
   currentPkmnInfo: previewInfo = {
     id: 1,
-    lang: 9
-  }
+    lang: 9,
+  };
 
   pkmn_stats!: number[];
   totalStats!: number;
 
-  constructor(private pokemonService: PokemonService) { }
+  constructor(private pokemonService: PokemonService) {}
 
   ngOnInit(): void {
-
-    this.listPokemon(this.page_info)
-    this.loadPokemon(this.currentPkmnInfo)
-
+    this.listPokemon(this.page_info);
+    this.loadPokemon(this.currentPkmnInfo);
   }
 
-  listPokemon(props : paginator): void {
-    this.pokemonService.getPokemonListResults( props )
-    .pipe(
-      take(1),
-      tap( ({ data }) => {
-        const { species } = data;
-        this.pokemonListSubject.next(species)
-      })
-    ).subscribe();
+  // listPokemon(props : paginator): void {
+  //   this.pokemonService.getPokemonListResults( props )
+  //   .pipe(
+  //     take(1),
+  //     tap( ({ data }) => {
+  //       const { species } = data;
+  //       this.pokemonListSubject.next(species)
+  //     })
+  //   ).subscribe();
+  // }
+
+  listPokemon(props: paginator): void {
+    this.pokemonService
+      .getPokemonListResults(props)
+      .pipe(
+        take(1),
+        switchMap(({ data }) => {
+          const { species } = data;
+          return of(species);
+        }),
+        tap((species) => {
+          this.pokemonListSubject.next(species as ListedPokemon['species']);
+        })
+      )
+      .subscribe();
   }
 
-  loadPokemon(props : previewInfo): void {
-    this.pokemonService.getPokemon( props )
-    .pipe(
-      take(1),
-      tap( ({ data }) => {
-        const { res } = data;
-        const pokemon: PreviewPokemon = {
-          id: undefined,
-          name: undefined,
-          genera: undefined,
-          height: undefined,
-          weight: undefined,
-          jap_name: undefined,
-          color: undefined,
-          region: undefined,
-          flavor: undefined,
-          types: [],
-          stats: []
-        };
-        pokemon.id = res[0].id;
-        pokemon.name = res[0].name;
-        pokemon.genera = res[0].specie.genera[0].genus;
-        pokemon.height = res[0].height / 10;
-        pokemon.weight = res[0].weight / 10;
-        pokemon.jap_name = res[0].specie.jap_name[0].name;
-        pokemon.color = res[0].specie.color.name;
-        pokemon.flavor = res[0].specie.flavor[0].text;
-        pokemon.types?.push
-        for (const type of res[0].types) {
-          pokemon.types?.push(type.type.name)
-        }
-        for (const stat of res[0].stats) {
-          pokemon.stats?.push({name: stat.stat.name, value: stat.value})
-        }
-        switch(res[0].specie.gen) {
-          case 1: pokemon.region = "Kanto"
-            break;
-          case 2: pokemon.region = "Jotoh"
-            break;
-          case 3: pokemon.region = "Hoenn"
-            break;
-          case 4: pokemon.region = "Sinnoh"
-            break;
-          case 5: pokemon.region = "Unova"
-            break;
-          case 6: pokemon.region = "Kalos"
-            break;
-          case 7: pokemon.region = "Alola"
-            break;
-          case 8: pokemon.region = "Galar"
-            break;
-          case 8: pokemon.region = "Paldea"
-            break;
-        }
-        this.previewPokemonSubject.next(pokemon)
-        switchBgByType(pokemon.types[0]);
-        [this.pkmn_stats, this.totalStats] = pkmnStatsToArray(pokemon.stats);
-      })
-    ).subscribe();
+  loadPokemon(props: previewInfo): void {
+    this.pokemonService
+      .getPokemon(props)
+      .pipe(
+        take(1),
+        tap(({ data }) => {
+          const { res } = data;
+          const pokemon: PreviewPokemon = {
+            id: undefined,
+            name: undefined,
+            genera: undefined,
+            height: undefined,
+            weight: undefined,
+            jap_name: undefined,
+            color: undefined,
+            region: undefined,
+            flavor: undefined,
+            types: [],
+            stats: [],
+          };
+          pokemon.id = res[0].id;
+          pokemon.name = res[0].name;
+          pokemon.genera = res[0].specie.genera[0].genus;
+          pokemon.height = res[0].height / 10;
+          pokemon.weight = res[0].weight / 10;
+          pokemon.jap_name = res[0].specie.jap_name[0].name;
+          pokemon.color = res[0].specie.color.name;
+          pokemon.flavor = res[0].specie.flavor[0].text;
+          pokemon.types?.push;
+          for (const type of res[0].types) {
+            pokemon.types?.push(type.type.name);
+          }
+          for (const stat of res[0].stats) {
+            pokemon.stats?.push({ name: stat.stat.name, value: stat.value });
+          }
+          switch (res[0].specie.gen) {
+            case 1:
+              pokemon.region = 'Kanto';
+              break;
+            case 2:
+              pokemon.region = 'Jotoh';
+              break;
+            case 3:
+              pokemon.region = 'Hoenn';
+              break;
+            case 4:
+              pokemon.region = 'Sinnoh';
+              break;
+            case 5:
+              pokemon.region = 'Unova';
+              break;
+            case 6:
+              pokemon.region = 'Kalos';
+              break;
+            case 7:
+              pokemon.region = 'Alola';
+              break;
+            case 8:
+              pokemon.region = 'Galar';
+              break;
+            case 8:
+              pokemon.region = 'Paldea';
+              break;
+          }
+          this.previewPokemonSubject.next(pokemon);
+          switchBgByType(pokemon.types[0]);
+          [this.pkmn_stats, this.totalStats] = pkmnStatsToArray(pokemon.stats);
+        })
+      )
+      .subscribe();
   }
 
   nextPokemon(): void {
     this.page_info.offset += 1;
     this.currentPkmnInfo.id += 1;
-    this.listPokemon(this.page_info)
-    this.loadPokemon(this.currentPkmnInfo)
+    this.listPokemon(this.page_info);
+    this.loadPokemon(this.currentPkmnInfo);
   }
 
   prevPokemon(): void {
     this.page_info.offset -= 1;
     this.currentPkmnInfo.id -= 1;
-    this.listPokemon(this.page_info)
-    this.loadPokemon(this.currentPkmnInfo)
+    this.listPokemon(this.page_info);
+    this.loadPokemon(this.currentPkmnInfo);
   }
 
   onPokemonSelected(id: number): void {
@@ -136,8 +167,7 @@ export class PokemonListComponent implements OnInit {
     if (this.currentPkmnInfo.id <= this.page_info.max - 10) {
       this.page_info.offset = id - 1;
     }
-    this.listPokemon(this.page_info)
-    this.loadPokemon(this.currentPkmnInfo)
+    this.listPokemon(this.page_info);
+    this.loadPokemon(this.currentPkmnInfo);
   }
-
 }
